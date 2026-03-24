11f85faeab9bac7d39b99bcc08e59b9c214b3d84
liguowang <liguowang01@countrygarden.com.cn>
2026-03-24 18:12:17 +0800

    修改配置文件

--- src/iot_comm/launch/iot_comm.sh
+++ src/iot_comm/launch/iot_comm.sh
@@ -2,8 +2,8 @@
 
 export ROS_DOMAIN_ID=5
 source /opt/ros/humble/setup.bash
-export ROS_LOCALHOST_ONLY=1
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI=file:///etc/cyclonedds.xml
 
 mkdir -m a=rwx -p /var/log/bzlrobot/iot_comm
 export ROS_LOG_DIR="/var/log/bzlrobot/iot_comm"
