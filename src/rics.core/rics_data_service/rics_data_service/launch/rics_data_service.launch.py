286e3afa9a1f1a4d01ca20bb6e480f0a686e8f9e
liguowang <liguowang01@countrygarden.com.cn>
2026-03-27 15:30:17 +0800

    从iot.yaml中提取did

--- src/rics.core/rics_data_service/rics_data_service/launch/rics_data_service.launch.py
+++ src/rics.core/rics_data_service/rics_data_service/launch/rics_data_service.launch.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
@@ -6,26 +7,28 @@ from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 def generate_launch_description():
 
-    # rics_data_service_container = ComposableNodeContainer(
-    #         name='rics_data_service_container',
-    #         namespace='',
-    #         package='rclcpp_components',
-    #         executable='component_container',
-    #         output='screen', 
-    #         composable_node_descriptions=[
-    #             ComposableNode(
-    #                 namespace='rics',
-    #                 package='rics_data_service',
-    #                 plugin='rics::data::RicsDataService',
-    #                 name='rics_data_service',
-    #                 parameters=[os.path.join(
-    #                     get_package_share_directory("rics_data_service"), 
-    #                     "config", "rics_data_service_launch.yaml")]
-    #                 ),
-    #         ]#,
-    # )
+    iot_config_file = os.path.join(get_package_share_directory("iot_comm"), "config", "iot_comm_params.yaml")
 
-    # return LaunchDescription([rics_data_service_container])
+    did = "SerialNumber"
+
+    # 提取SerialNumber的值
+    try:
+
+        # 读取YAML文件
+        with open(iot_config_file, 'r') as file:  # 替换为实际文件名
+            data = yaml.safe_load(file)
+
+        did = data['iot_comm']['ros__parameters']['did']
+        print(f"did: {did}")
+
+    except FileNotFoundError:
+        print("cannot find iot_config_file.")
+    except yaml.YAMLError as e:
+        print(f"YAMLError: {e}")
+    except KeyError as e:
+        print(f"cannot find did - {e}")
+    except Exception as e:
+        print(f"Exception: {e}")
 
     # 获取配置文件路径（增加文件存在性检查）
     config_file = os.path.join(
@@ -46,7 +49,7 @@ def generate_launch_description():
         # name='rics_data_service',
         namespace='rics',
         output='screen',
-        parameters=[config_file],
+        parameters=[config_file, {"RobotConfig.SerialNumber": did}],
         arguments=ros_args,  # 显式传递日志级别参数
     )
 
